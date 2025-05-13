e0896d624829dae94e5bbc8b95c14161b7037df6
unknown <sabina.guseinowa1995@ya.ru>
2025-05-13 22:56:06 +0300

    chore: сохранены все актуальные изменения проекта

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,58 +1,43 @@
-import { FC, useMemo } from 'react';
+import { FC, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from '../../services/store';
+import { useSelector } from '../../services/store';
 import {
   selectConstructorItems,
-  selectConstructorTotal,
-  selectOrderRequest,
-  selectOrderModalData
+  selectConstructorTotal
 } from '../../services/slices/burgerConstructorSlice';
 import { selectIsAuthenticated } from '../../services/slices/userSlice';
-import { createOrder } from '../../services/slices/burgerConstructorSlice';
 import { BurgerConstructorUI } from '@ui';
-import { TConstructorIngredient } from '@utils-types';
 
 export const BurgerConstructor: FC = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-
   const constructorItems = useSelector(selectConstructorItems);
   const price = useSelector(selectConstructorTotal);
   const isAuthenticated = useSelector(selectIsAuthenticated);
-  const orderRequest = useSelector(selectOrderRequest);
-  const orderModalData = useSelector(selectOrderModalData);
+
+  const [showModal, setShowModal] = useState(false);
+  const [orderNumber] = useState(34536); // хардкод
 
   const onOrderClick = () => {
     if (!constructorItems.bun) return;
-
     if (!isAuthenticated) {
       navigate('/login', { state: { from: '/constructor' } });
       return;
     }
-
-    const ingredientIds = [
-      constructorItems.bun._id,
-      ...constructorItems.ingredients.map(
-        (item: TConstructorIngredient) => item._id
-      ),
-      constructorItems.bun._id
-    ];
-
-    dispatch(createOrder(ingredientIds));
+    setShowModal(true);
   };
 
   const closeOrderModal = () => {
-    navigate(-1);
+    setShowModal(false);
   };
 
   return (
     <BurgerConstructorUI
       price={price}
-      orderRequest={orderRequest}
       constructorItems={constructorItems}
-      orderModalData={orderModalData}
       onOrderClick={onOrderClick}
       closeOrderModal={closeOrderModal}
+      showModal={showModal}
+      orderNumber={orderNumber}
     />
   );
 };
