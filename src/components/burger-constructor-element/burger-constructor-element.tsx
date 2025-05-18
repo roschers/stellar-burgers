7a41a4aee0c25301793f7141f3d174411a53e09e
unknown <sabina.guseinowa1995@ya.ru>
2025-05-18 18:54:30 +0300

    fix down and up buttons

--- src/components/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor-element/burger-constructor-element.tsx
@@ -2,15 +2,19 @@ import { FC, memo } from 'react';
 import { BurgerConstructorElementUI } from '@ui';
 import { BurgerConstructorElementProps } from './type';
 import { useDispatch } from '../../services/store';
-import { removeIngredient } from '../../services/slices/burgerConstructorSlice';
+import { removeIngredient, moveUpIngredient, moveDownIngredient } from '../../services/slices/burgerConstructorSlice';
 
 export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
   ({ ingredient, index, totalItems }) => {
     const dispatch = useDispatch();
 
-    const handleMoveDown = () => {};
+    const handleMoveDown = () => {
+      dispatch(moveDownIngredient(index));
+    };
 
-    const handleMoveUp = () => {};
+    const handleMoveUp = () => {
+      dispatch(moveUpIngredient(index));
+    };
 
     const handleClose = () => {
       dispatch(removeIngredient(ingredient));
